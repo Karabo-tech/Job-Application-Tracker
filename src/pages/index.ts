5335e48f14ebd7b6e5ec878770f3ea11cdd873ad
Karabo <karaboshabe226@gmail.com>
2025-08-10 22:52:54 +0200

    file structure for the pages

--- src/pages/index.ts
+++
@@ -1,6 +0,0 @@
-export { LandingPage } from './LandingPage';
-export { LoginPage } from './LoginPage';
-export { RegisterPage } from './RegisterPage';
-export { HomePage } from './HomePage';
-export { JobDetailPage } from './JobDetailPage';
-export { NotFoundPage } from './NotFoundPage';
